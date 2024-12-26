9bcd9d26cb7408c3f0e3d42b65d7a5aab276612a
Sneha Dharmalingam <sneha.dharma112@gmail.com>
2024-12-26 00:54:55 -0600

    Idek what changed tbh..

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
   styles: ``
 })
