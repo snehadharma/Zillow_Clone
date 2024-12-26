c1ccdc3145b053d06e56f9b054ddb93c57cd0726
Sneha Dharmalingam <sneha.dharma112@gmail.com>
2024-12-26 12:44:10 -0600

    Used ngFor to display multiple properties.

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -4,7 +4,7 @@ import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   template: `
     <section class="listing">
       <img
