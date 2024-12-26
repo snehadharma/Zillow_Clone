26d0b6e848416d5f3bf23672130b37bd28c0146a
Sneha Dharmalingam <sneha.dharma112@gmail.com>
2024-12-26 12:21:49 -0600

    Added property binding to components.

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -11,5 +13,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
